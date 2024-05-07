8e135195769ff8f3294dee50c2cd7bbad996a706
VovaMT <111053876+VovaMT@users.noreply.github.com>
2024-05-07 11:42:50 +0300

    fix4

--- src/main/java/lbgd/edu/ua/springsimpleservicedemo/controllers/TestResources.java
+++ src/main/java/lbgd/edu/ua/springsimpleservicedemo/controllers/TestResources.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestResources {
 
     @GetMapping("resources/{name}")
-    public String getName(@PathVariable String name){return "Hello laba"+ name;}
+    public String getName(@PathVariable String name){return "Hello laba "+ name;}
 }
